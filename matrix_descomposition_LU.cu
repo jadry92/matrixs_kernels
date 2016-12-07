07b7d1a6c3fb9a6fe377a1d6da75e32037c509c4
jadry92 <jhoanseb1992@gmail.com>
2016-12-07 10:22:17 -0500

    Version of today 12/06/2016

--- matrix_descomposition_LU.cu
+++ matrix_descomposition_LU.cu
@@ -2,9 +2,7 @@
 By : Johan S. Suarez L. or @jadry92 in twitter
 This kernel find the  determinte of the matrix A in variable B 
 
-
 */
-
 
 // Macro for mastrix index
 
@@ -45,10 +43,8 @@ int main(int argc, char **argv){
 	/* Initiation of variables*/
 	int i,j; // i == rows and j == colms
 	int N =3;
-	float* A;
-    float det;
-    int k;
-    int count = N;
+	float *A,*L,*U;
+
     struct timeval t_ini, t_fin;
     double time_full;   
 
@@ -56,6 +52,11 @@ int main(int argc, char **argv){
 
     A = (float*)malloc(N*N*sizeof(float));
     	checkMalloc(A,"A");
+    L = (float*)malloc(N*N*sizeof(float));
+        checkMalloc(L,"U");
+    U = (float*)malloc(N*N*sizeof(float));
+        checkMalloc(U,"U");        
+        
     float a[9]= {0,1,2,2,5,5,7,7,0};
     	/* Initialitation Matix A*/
 	memcpy(A,&a,N*N*sizeof(float));
@@ -70,16 +71,24 @@ int main(int argc, char **argv){
 
     printf("Strat Compute\n");
     gettimeofday(&t_ini, NULL);
-    det = Ind(A,0,0);
-    for (k = 0; k < count-1;++k){      
-        for(i = k+1; i < N; i++){
-            for(j = k+1; j < N; j++){
-                Ind(A,i,j)=(Ind(A,k,k)*Ind(A,i,j)-Ind(A,k,j)*Ind(A,i,k))/Ind(A,k,k);
+    Ind(L,0,0) = 1;     
+    for(i = 0; i < N; i++){
+        for(j = 0; j < N; j++){
+            if(i <= j){
+                Ind(U,i,j) = Ind(A,i,j);
             }
-            det = det*Ind(A,k+1,k+1);
-        }    
-//        det = det*Ind(A,k+1,k+1);
-    }
+            if(i >= j){
+                Ind(L,i,j) = Ind(A,i,j)/Ind(U,i,j);
+                Ind(U,i,j) = (Ind(A,i,j)-Ind(L,i,j)) 
+            }            
+        }
+        det = det*Ind(A,k+1,k+1);
+    }    
+
+
+
+
+
     gettimeofday(&t_fin, NULL);
     time_full = timeval_diff(&t_fin, &t_ini);   
     printf("\nTime of compute =%f [s] \n",time_full);
